aeb20f7acb5e50d0d882c2c56c9a8d840ddc0255
Pedro Pereira <pedro.2108@hotmail.com>
2024-10-26 18:35:05 -0300

    refactor: using get server side props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,34 @@
 import { NewsCard } from "@/components/NewsCard";
 import { News } from "@/types/news";
 
-export default async function Home() {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/news`);
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/news`);
+  const data = await res.json();
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  return { props: { news: data } };
+}
 
-    const news: News[] = await response.json();
+export default async function Home({ news }: { news: News[] }) {
 
-    if (!news || news.length === 0) {
-      return (
-        <main className="p-4">
-          <h1 className="text-2xl font-bold mb-4">News Crawler</h1>
-          <section>
-            <p>No news found</p>
-          </section>
-        </main>
-      );
-    }
-
-    return (
-      <main className="p-4">
-        <h1 className="text-2xl font-bold mb-4">News Crawler</h1>
-        <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {news.map(report => (
-            <NewsCard key={report.id} news={report} />
-          ))}
-        </section>
-      </main>
-    );
-  } catch (error) {
-    console.error('Error:', error);
+  if (!news || news.length === 0) {
     return (
       <main className="p-4">
         <h1 className="text-2xl font-bold mb-4">News Crawler</h1>
         <section>
-          <p className="text-red-500">Error loading news</p>
-          {JSON.stringify(error)}
+          <p>No news found</p>
         </section>
       </main>
     );
   }
+
+  return (
+    <main className="p-4">
+      <h1 className="text-2xl font-bold mb-4">News Crawler</h1>
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {news.map(report => (
+          <NewsCard key={report.id} news={report} />
+        ))}
+      </section>
+    </main>
+  );
 }
